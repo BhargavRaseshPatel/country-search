79218f578427246e504499db46bd7890e52df69d
Bhargav Patel <bhargavrpatelcanada@gmail.com>
2025-09-06 17:58:22 -0400

    resolve the error

--- src/Card.jsx
+++ src/Card.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Card = ({name, image}) => {
   return (
-    <div style={{border : '1px solid gray', borderRadius : '8px', width: 'min-content', padding : '16px', margin: '16px', marginBottom : '0px'}}>
+    <div className="countryCard" style={{border : '1px solid gray', borderRadius : '8px', width: 'min-content', padding : '16px', margin: '16px', marginBottom : '0px'}}>
         <img src={image} width={175} height={100}  />
         <h2 style={{margin: '0px'}}>{name}</h2>
     </div>
